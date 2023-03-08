a68972cad0b7cb43229dd6c4b24213cf69695b3a
Kyaw Swar Thant <kyawswar.thant25@gmail.com>
2023-03-08 08:31:35 +0630

    [add] validation

--- app/concepts/admin/registrations/contract/create.rb
+++ app/concepts/admin/registrations/contract/create.rb
@@ -4,12 +4,14 @@ module Admin::Registrations::Contract
     properties :email, :password, :password_confirmation
 
     validates :email, presence: true
+    validates :password, presence: true, if: -> { self.email.present? }
+    validates :password_confirmation, presence: true, if: -> { self.password.present? }
 
-    validate :check_password
+    validate :check_password, if: -> { password.present? && password_confirmation.present? }
 
     def check_password
       if password != password_confirmation
-        errors.add(:password, "password not same")
+        errors.add(:passwords, "not same")
       end
     end
   end
